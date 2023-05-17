1091ec92ef68110db6a369c22425bf1997f2d75c
levymc <levymcruz@gmail.com>
2023-05-16 21:37:52 -0300

    initial config

---
+++ src/Container.jsx
@@ -0,0 +1,15 @@
+// Importando o React e o styled-component Container
+import React from 'react';
+import styled from 'styled-components';
+
+// Definindo o componente Estilo
+export function Estilo() {
+  return (
+    <Container>OOOOO</Container>
+  )
+}
+
+// Configurando o styled-component Container
+const Container = styled.div`
+  background-color: red;
+`;
