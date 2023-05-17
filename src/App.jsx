286a5a76b600c6fa37048dbaf62c2db2956e5203
levymc <levymcruz@gmail.com>
2023-05-16 21:56:47 -0300

    testando o styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
 
   return (
     <>
-      <Estilo></Estilo>
+      <Estilo>
+
+      </Estilo>
     </>
   )
 }
--- src/Container.jsx
+++ src/Container.jsx
@@ -5,11 +5,31 @@ import styled from 'styled-components';
 // Definindo o componente Estilo
 export function Estilo() {
   return (
-    <Container>OOOOO</Container>
+    <Container>
+        OOOOO
+        <MeuBotao id="btn1">Botao</MeuBotao>
+    </Container>
+    
   )
 }
 
 // Configurando o styled-component Container
 const Container = styled.div`
   background-color: red;
+  width: 100%;
+  height: 100%;
+
+  #btn1{
+    background-color: yellow;
+  }
+`;
+
+const MeuBotao = styled.button`
+    background: #0099FF;
+    border: 2px solid #0099FF;
+    border-radius: 3px;
+    color: #FDFDFD;
+    font-size: 1.2rem;
+    margin: 1rem;
+    padding: 1rem 1.5rem;
 `;
