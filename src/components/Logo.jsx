687264f11a74724ad095f19a41987b76a42f4612
levymc <levymcruz@gmail.com>
2023-05-17 19:10:42 -0300

    adicionando o Raio.svg

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,20 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import raio from "../assets/img/raio.svg"
 
 export default function Logo(){
     return (
-        <SCLogo>ZapRecall</SCLogo>
+        <SCLogo>
+            <img src={raio} alt="ThunderZap" />
+            <h2>ZapRecall</h2>
+        </SCLogo>
     )
 }
 
 const SCLogo = styled.div`
+    padding-top: 1em; padding-bottom: 0.5em;
+    display:flex;
+    gap: 1em;
+    align-items: center;
     font-family: 'Righteous';
     font-style: normal;
     font-weight: 400;
